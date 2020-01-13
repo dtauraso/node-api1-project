e30fa8afba9f6a77e2dd3039731cf6e95937da85
David Tauraso <david318@me.com>
2020-01-13 13:28:43 -0800

    update works

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ server.get('/api/users', (req, res) => {
     Users.find() // return a promise
         .then(users => {
             // always return a status
-            console.log(users)
+            // console.log(users)
             res.status(200).json(users)
         })
         .catch(err => {
             // console.log(err)
-            res.status(500).json({ errorMessage: 'couldn\'t get the list of users'})
+            res.status(500).json({ errorMessage: "The users information could not be retrieved."})
         })
 }) // api is the location of all hub operations
 // findById(id)
@@ -40,11 +40,17 @@ server.get('/api/users/:id', (req, res) => {
     // the id in the axios call
     Users.findById(req.params.id)
         .then(user => {
-            res.status(200).json(user)
+            if(!user) {
+                res.status(404).json({errorMessage: "The user with the specified ID does not exist."})
+            } else {
+
+                res.status(200).json(user)
+
+            }
         })
         .catch(err => {
             // console.log(err)
-            res.status(500).json({ errorMessage: 'couldn\'t get the user'})
+            res.status(500).json({ errorMessage: "The user information could not be retrieved."})
         })
 })
 // insert(user)
@@ -53,7 +59,11 @@ server.get('/api/users/:id', (req, res) => {
 server.post('/api/users', (req, res) => {
     const userData = req.body
 
+    const keys = Object.keys(userData)
+    if(!keys.includes("name") || !keys.includes("bio")) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
 
+    }
     // never trust the client, validate the data.  for now we trust the client for the demo
     Users.insert(userData)
         .then(users => {
@@ -61,23 +71,56 @@ server.post('/api/users', (req, res) => {
         })
         .catch(err => {
             // console.log(err)
-            res.status(500).json({ errorMessage: 'couldn\'t create the user'})
+            res.status(500).json({ errorMessage: "There was an error while saving the user to the database"})
         })
 })
 // update(id, user)
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    const updatedUser = req.body
 
+    const keys = Object.keys(updatedUser)
+    if(!keys.includes("name") || !keys.includes("bio")) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+
+    }
+
+    Users.update(id, updatedUser)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({errorMessage: "The user with the specified ID does not exist."})
+
+            } else {
+                res.status(200).json(user)
+
+            }
+
+        })
+        .catch(err => {
+            // console.log(err)
+            res.status(500).json({ errorMessage: 'couldn\'t update the user'})
+        })
+
+})
 // remove(id)
 // delete a Hub
 server.delete('/api/users/:id', (req, res) => {
     const id = req.params.id
     Users.remove(id)
         .then(result => {
-            // only sent a 1 back
-            res.status(200).json(result)
+            if(!result) {
+                res.status(404).json({errorMessage: "The user with the specified ID does not exist."})
+
+            }
+            else {
+                // only sent a 1 back
+                res.status(200).json(result)
+
+            }
         })
         .catch(err => {
             // console.log(err)
-            res.status(500).json({ errorMessage: 'couldn\'t delete the user'})
+            res.status(500).json({ errorMessage: "The user could not be removed"})
         })
 })
 // update a Hub
