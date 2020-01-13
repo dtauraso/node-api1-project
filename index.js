8024f1b542e58b728a2c99a51356952debb68737
David Tauraso <david318@me.com>
2020-01-13 11:57:01 -0800

    Started project

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+// import express from 'express'; // ES2015 module syntax
+
+const express = require('express') // CommonJS modules
+
+const Hubs = require('./data/db.js') // our Hubs database library
+
+
+const server = express()
+
+
+// middleware teaches express to do new things
+server.use(express.json());   // needs to parse json
+
+// routes or endpoints
+
+// GET to "/"
+server.get('/', (req, res) => {
+    res.send({hello: 'Web 25!'})
+});
+
+
+const port = 8001
+server.listen(port, () => {console.log(`\n ** api on port: ${port} **`)})
